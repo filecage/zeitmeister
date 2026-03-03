213b0e6fa13382918d6e27097180a37054918cb1
David <filecage@users.noreply.github.com>
2026-03-03 18:13:56 +0100

    separate types of calendar config

--- src/types/CalendarConfig.ts
+++ src/types/CalendarConfig.ts
@@ -1,4 +1,4 @@
-type CalendarConfig = {
+type CalendarConfigBase = {
     /**
      * A unique identifier for the calendar. Only required if the calendar is referenced.
      */
@@ -8,7 +8,33 @@ type CalendarConfig = {
      * Type of the calendar.
      * Be aware that `ical` cannot be used as Scheduling calendar because it's read-only
      */
-    adapter: 'caldav' | 'googlecalendar' | 'ical',
+    adapter: string,
+
+    /**
+     * Server URI of the calendar
+     */
+    server: string,
+
+    /**
+     * URIs of the exact calendars that you want to check for
+     * Can be relative to the server URI
+     */
+    uris: string[],
 };
+
+type CalDavCalendarConfig = Omit<CalendarConfigBase, 'adapter'> & {
+    adapter: 'caldav',
+
+    /**
+     * Authentication credentials
+     * Currently only username/password (HTTP basic auth) supported
+     */
+    auth: {
+        username: string,
+        password: string,
+    }
+}
+
+type CalendarConfig = CalDavCalendarConfig;
 
 export default CalendarConfig;
