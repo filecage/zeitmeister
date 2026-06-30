003727eb4b68353237fa8f3cd89b3c23a80d2186
David <filecage@users.noreply.github.com>
2026-06-30 15:11:54 +0200

    use seconds instead of minutes for chunking

--- src/util/chunkTimeslot.test.ts
+++ src/util/chunkTimeslot.test.ts
@@ -5,28 +5,28 @@ test('Should chunk the timeslot from 10:30 to 17:00 in 13 chunks of 30 minutes',
     expect([...chunkTimeslot({
         start: new Date('2026-06-30T10:30:00Z'),
         end: new Date('2026-06-30T17:00:00Z')
-    }, 30)]).toMatchSnapshot();
+    }, 30 * 60)]).toMatchSnapshot();
 });
 
 test('Should chunk the timeslot from 10:30 to 17:00 in 9 chunks of 30 minutes with 15 minutes padding', () => {
     expect([...chunkTimeslot({
         start: new Date('2026-07-01T10:30:00Z'),
         end: new Date('2026-07-01T17:00:00Z')
-    }, 30, 15)]).toMatchSnapshot();
+    }, 30 * 60, 15 * 60)]).toMatchSnapshot();
 })
 
 test('Should chunk the timeslot over multiple days and months', () => {
     expect([...chunkTimeslot({
         start: new Date('2026-06-30T10:30:00Z'),
         end: new Date('2026-07-02T17:00:00Z')
-    }, 360, 10)]).toMatchSnapshot();
+    }, 360 * 60, 10 * 60)]).toMatchSnapshot();
 });
 
 test('Should chunk zero-length chunks', () => {
     expect([...chunkTimeslot({
         start: new Date('2026-06-30T10:00:00Z'),
         end: new Date('2026-06-30T10:05:00Z')
-    }, 0, 1)]).toMatchSnapshot();
+    }, 0, 60)]).toMatchSnapshot();
 
 })
 
--- src/util/chunkTimeslot.ts
+++ src/util/chunkTimeslot.ts
@@ -1,8 +1,8 @@
 import Timeslot from "@/zeitmeister/types/Timeslot";
-import {addMinutes} from "date-fns";
+import {addSeconds} from "date-fns";
 
-export function *chunkTimeslot (timeslot: Timeslot, chunkLengthInMinutes: number, paddingInMinutes: number = 0) : Generator<Timeslot> {
-    if (chunkLengthInMinutes === 0 && paddingInMinutes === 0) {
+export function *chunkTimeslot (timeslot: Timeslot, chunkLengthInSeconds: number, paddingInSeconds: number = 0) : Generator<Timeslot> {
+    if (chunkLengthInSeconds === 0 && paddingInSeconds === 0) {
         throw new Error(`Can not chunk timeslot if chunk length and padding is zero`);
     }
 
@@ -10,12 +10,12 @@ export function *chunkTimeslot (timeslot: Timeslot, chunkLengthInMinutes: number
 
     while (cursor < timeslot.end) {
         const start = new Date(cursor);
-        const end = addMinutes(cursor, chunkLengthInMinutes);
+        const end = addSeconds(cursor, chunkLengthInSeconds);
 
         if (end <= timeslot.end) {
             yield {start, end};
         }
 
-        cursor.setMinutes(cursor.getMinutes() + chunkLengthInMinutes + paddingInMinutes);
+        cursor.setTime(cursor.getTime() + ((chunkLengthInSeconds + paddingInSeconds) * 1000));
     }
 }
